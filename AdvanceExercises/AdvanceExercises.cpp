d25eb56a54e6b0d79457ee0cec32968d1f854f86
Romanov <konstantins.romanovs@accenture.com>
2022-03-21 19:56:32 +0200

    Implements partially base class

--- AdvanceExercises/AdvanceExercises.cpp
+++ AdvanceExercises/AdvanceExercises.cpp
@@ -2,10 +2,77 @@
 //
 
 #include <iostream>
+#include <string>
+using namespace std;
+
+class Package {
+
+private:
+	string m_recip_name;
+	string m_recip_address;
+	string m_recip_city;
+	string m_recip_state;
+	string m_recip_zip;
+
+	string m_send_name;
+	string m_send_address;
+	string m_send_city;
+	string m_send_state;
+	string m_send_zip;
+
+	double m_weight;
+	double m_cost;
+
+public:
+
+	Package(string recip_name,
+		string recip_address,
+		string recip_city,
+		string recip_state,
+		string recip_zip,
+		string send_name,
+		string send_address,
+		string send_city,
+		string send_state,
+		string send_zip,
+		double weight,
+		double cost
+	) {
+		m_recip_name = recip_name;
+		m_recip_address = recip_address;
+		m_recip_city = recip_city;
+		m_recip_state = recip_state;
+		m_recip_zip = recip_zip;
+
+		m_send_name = send_name;
+		m_send_address = send_address;
+		m_send_city = send_city;
+		m_send_state = send_state;
+		m_send_zip = send_zip;
+
+		m_weight = weight;
+		m_cost = cost;
+	}
+
+	double calculateCost() {
+		double result = m_weight * m_cost;
+
+		return result;
+	}
+};
+
+
 
 int main()
 {
-    std::cout << "Hello World!\n";
+
+	Package package1 = Package("1", "Address 1", "Riga", "Latvia", "LV-1010",
+		"1", "Address 1", "Riga", "Latvia", "LV-1010", 1.5, 10.10);
+
+	double cost1 = package1.calculateCost();
+
+
+	//std::cout << "Hello World!\n";
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
