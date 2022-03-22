1071e8aaadde88f30b3d09d92997ced5adcf9ea2
Romanov <konstantins.romanovs@accenture.com>
2022-03-22 10:20:55 +0200

    Create print table function

--- AdvanceExercises/AdvanceExercises.cpp
+++ AdvanceExercises/AdvanceExercises.cpp
@@ -7,12 +7,16 @@
 #include "TwoDayPackage.h"
 #include "OvernightPackage.h"
 #include <vector>
+#include <iomanip> 
 using namespace std;
+
+void printTestTable(vector<Package*> packages);
+void printLine(int lenght);
 
 int main()
 {
-	Package package1 = Package("1", "Address 1", "Riga", "Latvia", "LV-1010",
-		"1", "Address 1", "Riga", "Latvia", "LV-1010", 1.5, 10.10);
+	Package package1 = Package("Robert", "Jelgavas 10", "Riga", "Latvia", "LV-1010",
+		"Julia", "Avotu", "Riga", "Latvia", "LV-1020", 1.5, 10.10);
 	Package package2 = Package("1", "Address 1", "Riga", "Latvia", "LV-1010",
 		"2", "Address 2", "Riga", "Latvia", "LV-1010", 3.5, 13.10);
 
@@ -32,6 +36,32 @@ int main()
 	packages.push_back(&package2);
 	packages.push_back(&twoDayPackage1);
 
+	printTestTable(packages);	
+}
+
+void printTestTable(vector<Package*> packages) {
+
+	int lineWidth = 220;
+	printLine(lineWidth);
+
+	cout
+		<< left << setw(20) << "Recipient Name"
+		<< setw(20) << "Recipient Address"
+		<< setw(20) << "Recipient City"
+		<< setw(20) << "Recipient State"
+		<< setw(20) << "Recipient ZIP"
+
+		<< setw(20) << "Sender Name"
+		<< setw(20) << "Sender Address"
+		<< setw(20) << "Sender City"
+		<< setw(15) << "Sender State"
+		<< setw(10) << "Sender ZIP"
+
+		<< setw(20) << "Weight"
+		<< setw(15) << "Cost" << endl;
+		
+	printLine(lineWidth);
+
 	double totalCosts{};
 
 	for (size_t i = 0; i < packages.size(); i++)
@@ -39,20 +69,32 @@ int main()
 		double currentCost = packages[i]->calculateCost();
 
 		cout
-			<< "Recipient Address: "
-			<< packages[i]->getRecipAddress() << "   "
-			<< "Sender Address: "
-			<< packages[i]->getSendAddress() << "   "
-			<< "Costs: "
-			<< currentCost << endl;
+			<< left << setw(20) << packages[i]->getRecipName()
+			<< setw(20) << packages[i]->getRecipAddress()
+			<< setw(20) << packages[i]->getRecipCity()
+			<< setw(20) << packages[i]->getRecipState()
+			<< setw(20) << packages[i]->getRecipZip()
+
+			<< setw(20) << packages[i]->getSendName()
+			<< setw(20) << packages[i]->getSendAddress()
+			<< setw(20) << packages[i]->getSendCity()
+			<< setw(15) << packages[i]->getSendState()
+			<< setw(10) << packages[i]->getSendZip()
+
+			<< setw(20) << packages[i]->getWeight()
+			<< setw(15) << packages[i]->getCost() << endl;
 
 		totalCosts += currentCost;
 	}
 
+	printLine(lineWidth);
 	cout << "Total costs: " << totalCosts << endl;
+	printLine(lineWidth);
 }
 
-
+void printLine(int lenght) {
+	cout << string(lenght, '-') << endl;
+}
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
 // Debug program: F5 or Debug > Start Debugging menu
